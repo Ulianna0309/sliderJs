637ff081b315b83d4cc5e5c0f613f3add96a15e2
Ulianna0309 <ulianna0309@gmail.com>
2020-11-15 18:59:25 +0200

    from lesson

--- main.js
+++ main.js
@@ -1,88 +1,84 @@
-let slides = document.querySelectorAll('.slide');
+const slides = document.querySelectorAll('.slide');
+const indicatorsContainer = document.querySelector('.indicators-container');
+const indicators = document.querySelectorAll('.indicator');
+const pauseBtn = document.querySelector('#pause');
+const prevBtn = document.querySelector('#previous');
+const nextBtn = document.querySelector('#next');
+
+
 let currentSlide = 0;
-let slideInterval = setInterval(nextSlide, 2000);
-let playing = true;
-let pauseButton = document.querySelector('#pause');
-let next = document.querySelector('#next');
-let previous = document.querySelector('#previous');
-let controls = document.querySelectorAll('.controls');
-let indicators = document.querySelectorAll('.indicator');
-let indicatorsContainer = document.querySelectorAll('#indicators-container');
+let interval = 2000;
+let slidesCount = slides.length;
+let intervalID = null;
+let isPlaying = true;
 
-function nextSlide() {
-goToSlide(currentSlide + 1);
+function goToSlide(n){
+    slides[currentSlide].classList.toggle('active');
+    indicators[currentSlide].classList.toggle('active');
+    currentSlide = (n + slidesCount) % slidesCount;
+    slides[currentSlide].classList.toggle('active');
+    indicators[currentSlide].classList.toggle('active');
 }
 
-function prevSlide() {
-goToSlide(currentSlide - 1);
+function goToPrev(){
+  goToSlide(currentSlide - 1);
 }
 
-function goToSlide(n) {
-slides[currentSlide].className = 'slide';
-currentSlide = (n + slides.length) % slides.length;
-slides[currentSlide].className = 'slide active';
-indicators[currentSlide].className = 'indicator';
-indicators[currentSlide].className = 'indicator active';
+function goToNext(){
+  goToSlide(currentSlide + 1);   
 }
 
-function pauseSlide() {
-    pauseButton.innerHTML = '<i class="far fa-play-circle"></i>';
-    playing = false;
-    clearInterval(slideInterval);
+function prevSlide(){
+  goToPrev();
+  pause();
 }
 
-function playSlide() {
-    pauseButton.innerHTML = '<i class="far fa-pause-circle"></i>';
-    playing = true;
-    slideInterval = setInterval(nextSlide, 2000);
+function nextSlide(){  
+  goToNext();
+  pause();
 }
+
+function play(){
+    intervalID = setInterval(goToNext, interval);
+    pauseBtn.innerHTML = '<i class="far fa-pause-circle"></i>'; 
+    isPlaying = true;
+}
+
+function pause(){
+    if(isPlaying){
+    clearInterval(intervalID);
+    pauseBtn.innerHTML = '<i class="far fa-play-circle"></i>'; 
+    isPlaying = false; 
+  }
+}
+
+function pausePlay(){
+    if (isPlaying) pause();
+    else play(); 
+}
+
+
 function indicate(e){
     let target = e.target;
-    if (target.classList.contains('indicator')){
-    pauseSlide();
-    goToSlide(target.getAttribute('data-slide-to'));
-
-    }
+    if(target.classList.contains('indicator')){
+     pause();
+     goToSlide(+target.getAttribute('data-slide-to'));  
+  }
 }
+
 function pressKey(e){
-    //e.code
-    //ArrowLeft
-    //ArrowRight
-    //Space
-    if(e.code === "ArrowLeft") prevSlide();
-    if(e.code === "ArrowRight") nextSlide();
-    if(e.code === "Space") pauseSlide() ;
+    if(e.code ==='ArrowLeft') prevSlide();
+    if(e.code ==='ArrowRight') nextSlide();
+    if(e.code === 'Space') pausePlay();
 }
 
-pauseButton.onclick = function() {
-    if (playing) {
-  	    pauseSlide();
-    } else {
-  	    playSlide();
-	  }
-};
-
-next.onclick = function () {
-pauseSlide();
-nextSlide();
-};
-
-previous.onclick = function () {
-pauseSlide();
-prevSlide();
-};
 
 
+pauseBtn.addEventListener('click', pausePlay);
+prevBtn.addEventListener('click', prevSlide);
+nextBtn.addEventListener('click', nextSlide);
+indicatorsContainer.addEventListener('click', indicate);
 document.addEventListener('keydown', pressKey);
 
-for (let i = 0; i < controls.length; i++){
-controls[i].style.display = 'inline-block';
-}
-for (let i = 0; i < indicators.length; i++){
-indicators[i].addEventListener('click', indicate);
-}
-
-
-
-
+intervalID = setInterval(goToNext, interval);
 
